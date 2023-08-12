5a3b6756f4e845d7b5b0a455d720c5059f59d6eb
Vinodh Kumar <vinodhkumar.manickam@freshworks.com>
2023-08-12 15:25:41 +0530

    feat: styling support

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <div>
+      <div className="bg-slate-500">
         <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
